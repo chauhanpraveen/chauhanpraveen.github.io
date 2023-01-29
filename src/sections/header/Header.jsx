52d80f2ca1d4d38025a22fb54ea5c440a8149f95
chauhanpraveen <chauhanpraveensingh303@gmail.com>
2023-01-29 16:46:18 +0530

    Created Header Section

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,31 @@
 import './header.css'
+import HeaderImage from '../../assets/headerpraveen.jpg'
+import data from './data'
 
 const Header = () => {
   return (
-    <section id="header"></section>
+    <header id='header'>
+      <div className='container header__container'>
+        <div className='header__profile'>
+          <img src={HeaderImage} alt='Header Portait' />
+        </div>
+        <h3>Praveen Singh Chauhan</h3>
+        <p>
+          Eager to work in challenging positions where my problem-solving skills, with a strong desire to learn new prospects to help the organization in the achievement of its goal.
+        </p>
+        <div className='header__cta'>
+          <a href="#contact" className='btn primary'>Let's Talk</a>
+          <a href="#portfolio" className='btn'>My Work</a>
+        </div>
+        <div className="header__socials">
+          {
+            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferror">{item.icon}</a>)
+          }
+        </div>
+
+
+      </div>
+    </header>
   )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,17 @@
+
+import { AiOutlineLinkedin } from 'react-icons/ai'
+import { AiOutlineGithub } from 'react-icons/ai'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiOutlineFacebook } from 'react-icons/ai'
+
+
+const data = [
+    { id: 1, link: "https://www.linkedin.com/in/praveen-singh-chauhan-73b949251/", icon: <AiOutlineLinkedin /> },
+    { id: 2, link: "https://github.com/chauhanpraveen", icon: <AiOutlineGithub /> },
+    { id: 3, link: "https://praveeninstragram.com", icon: <AiOutlineInstagram /> },
+    { id: 4, link: "https://www.praveensingh.com", icon: <AiOutlineTwitter /> },
+    { id: 5, link: "https://www.facebook.com/jayingh.chauhan", icon: <AiOutlineFacebook /> }
+
+]
+export default data
