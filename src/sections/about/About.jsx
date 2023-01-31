d4b57639ee8ca2b3ff6c491187324a6efad79681
chauhanpraveen <chauhanpraveensingh303@gmail.com>
2023-01-31 22:01:39 +0530

    added services section

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,12 +1,46 @@
-
-import './about.css';
+import AboutImage from "../../assets/praveensingh.jpg";
+import CV from "../../assets/update1resume.docx";
+import { HiDownload } from "react-icons/hi";
+import data from "./data";
+import Card from "../../components/Card";
+import "./about.css";
 
 const About = () => {
-    return (
-        <section id='about'>
-            <h2>About</h2>
-        </section>
-    )
-}
+  return (
+    <section id="about">
+      <div className="container about__container">
+        <div className="about__left">
+          <div className="about__portrait">
+            <img src={AboutImage} alt="About Image" />
+          </div>
+        </div>
 
-export default About
+        <div className="about__right">
+          <h2>About Me</h2>
+          <div className="about__cards">
+            {
+              data.map((item) => (
+              <Card key={item.id} className="about__card">
+                <span className="about__card-icon">{item.icon}</span>
+                <h5>{item.title}</h5>
+                <small>{item.desc}</small>
+              </Card>
+            ))}
+          </div>
+          <p>
+            Hi my name is Praveen Singh Chauhan from Bangalore. I'm a full-stack
+            web developer with a Bachelors degree in Computer. My top priority
+            is to get your bussines online the right way,gicitry
+          </p>
+          <p>Hi im A full-stack Developer</p>
+          <a href={CV} download className="btn primary">
+            Download CV
+            <HiDownload />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/sections/about/data.js
@@ -0,0 +1,11 @@
+import {FaAward} from 'react-icons/fa'
+import {TbBooks} from 'react-icons/tb'
+import {BiHappyHeartEyes} from 'react-icons/bi'
+
+const data = [
+    {id: 1, icon:<FaAward/>, title:'Experience', desc: '2+ Years Working'},
+    {id: 2, icon:<TbBooks/>, title:'Projects', desc: '10+ Projects Completed'},
+    {id: 3, icon:<BiHappyHeartEyes/>, title:'Clients', desc: '10+ Happy Clients'}
+]
+
+export default data
